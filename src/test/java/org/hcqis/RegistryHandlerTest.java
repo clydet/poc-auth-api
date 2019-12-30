b0b0a71b3e5ff68b4ec420b0398393cbbb61e6b3
Clyde Tedrick <iamclyde@gmail.com>
2019-12-29 19:29:47 -0500

    issue-1: project structure

---
+++ src/test/java/org/hcqis/RegistryHandlerTest.java
@@ -0,0 +1,14 @@
+package org.hcqis;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class RegistryHandlerTest {
+
+  @Test
+  public void testJWTCreation() {
+    RegistryHandler handler = new RegistryHandler();
+    assertTrue(handler.createJWT());
+  }
+}
