20eadef40a6e00772e834ef81a2e5c74dd9baece
Clyde Tedrick <iamclyde@gmail.com>
2020-01-07 14:55:26 -0500

    add sample api handler

---
+++ src/main/java/org/hcqis/SampleAppHandler.java
@@ -0,0 +1,26 @@
+package org.hcqis;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class SampleAppHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = LogManager.getLogger(SampleAppHandler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: {}", input);
+		Response responseBody = new Response("You have authenticated successfully!", input);
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build();
+	}
+}
